ef487ac55f0bb9aa41fea809b6c4a5e788fd69f9
Damian <damian.duda2302@gmail.com>
2024-01-08 13:18:21 +0100

    Nav fixed and HowToJoin Added

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,9 @@ const optionsContainer = document.querySelector('.nav__settings-options');
 const options = document.querySelectorAll('.nav__settings-option');
 const navMiddle = document.querySelector('.nav__middle');
 const navMiddleLinks = document.querySelector('.nav__middle-links');
+const howToJoinBtnLeft = document.querySelector('.howToJoin__choice-left');
+const howToJoinBtnRight = document.querySelector('.howToJoin__choice-right');
+const noActionElements = document.querySelectorAll('#noAction');
 
 const menuHandler = () => {
 	menu.classList.toggle('nav__mobile--active');
@@ -35,16 +38,44 @@ const optionsHandler = () => {
 };
 
 const navChangeBgc = () => {
-	if (window.scrollY >= 80) {
-		navMiddle.classList.add('nav__middle--scrolled');
-		navMiddleLinks.classList.add('nav__middle-links--scrolled');
+	if (window.innerWidth >= 772) {
+		if (window.scrollY >= 80) {
+			navMiddle.classList.add('nav__middle--scrolled');
+			navMiddleLinks.classList.add('nav__middle-links--scrolled');
+		} else {
+			navMiddle.classList.remove('nav__middle--scrolled');
+			navMiddleLinks.classList.remove('nav__middle-links--scrolled');
+		}
 	} else {
-		navMiddle.classList.remove('nav__middle--scrolled');
-		navMiddleLinks.classList.remove('nav__middle-links--scrolled');
+		if (window.scrollY >= 40) {
+			nav.classList.add('nav--scrolled');
+		} else {
+			nav.classList.remove('nav--scrolled');
+		}
 	}
 };
+
+const howToJoinActionLeft = () => {
+	howToJoinBtnLeft.classList.toggle('howToJoin__choice--actionReavelLeft');
+	howToJoinBtnRight.classList.toggle('howToJoin__choice--actionHideRight');
+};
+
+const howToJoinActionRight = () => {
+	howToJoinBtnRight.classList.toggle('howToJoin__choice--actionReavelRight');
+	howToJoinBtnLeft.classList.toggle('howToJoin__choice--actionHideLeft');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+	noActionElements.forEach(function (noActionElement) {
+		noActionElement.addEventListener('click', function (event) {
+			event.stopPropagation();
+		});
+	});
+});
 
 burgerBtn.addEventListener('click', menuHandler);
 langBtn.addEventListener('click', langHandler);
 profileBtn.addEventListener('click', optionsHandler);
 window.addEventListener('scroll', navChangeBgc);
+howToJoinBtnLeft.addEventListener('click', howToJoinActionLeft);
+howToJoinBtnRight.addEventListener('click', howToJoinActionRight);
